d4b91ee2c45622378dc71613746ce1179f92574b
Khidirov Ali <alihidirov285@gmail.com>
2025-11-10 21:40:32 +0600

    sozlandi

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'maxway',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'maxway.db',
+        'USER': 'ali',
+        'PASSWORD': '090926',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
