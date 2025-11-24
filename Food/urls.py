44b4d4b6af19cdcba6692c219ac8eb8deba2679f
Khidirov Ali <alihidirov285@gmail.com>
2025-11-24 22:50:39 +0600

    yangilanish kiritildi

--- Food/urls.py
+++ Food/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu, name='food_menu'),
+    path('', views.home_page, name='food_menu'),
+    path('order_page/', views.order_page, name='order_page'),
+    path('index/', views.index, name='index'),
+    path('main_order/', views.main_order, name='main_order'),
 ]
--- Food/views.py
+++ Food/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from services import *
-from models import *
+from .services import *
+from .models import *
 import json
-from models import *
+from .models import *
 from django.shortcuts import redirect,render
 from django.http import JsonResponse
 from config.settings import MEDIA_ROOT
@@ -11,3 +11,17 @@ def home_page(request):
     if request.GET:
         product = get_product_by_id(request.GET.get("product_id", 0))
         return JsonResponse(product)
+
+def order_page(request):
+
+    return render(request, "food/order.html")
+
+
+def index(request):
+
+    return render(request, "food/index.html")
+
+
+def main_order(reques):
+
+    return render(reques, "food/order.html")
